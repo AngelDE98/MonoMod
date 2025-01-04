3d7e9000aed671470746cdda4b5670adce35d019
Andreas Pardeike <andreas@pardeike.net>
2025-01-04 23:40:10 +0100

    adds support for nullable<t> return types to the abi

--- src/MonoMod.UnitTest/RuntimeDetour/ReturnBufferTest.cs
+++ src/MonoMod.UnitTest/RuntimeDetour/ReturnBufferTest.cs
@@ -26,6 +26,35 @@ namespace MonoMod.UnitTest
             Assert.True(Source(0, 0, 0, 0) is { f1: 4, f2: 5, f3: 6 });
         }
 
+        [Fact]
+        public void TestReturnBufferNullableDetour()
+        {
+            Assert.True(Source(0, 0, 0, 0) is { f1: 1, f2: 2, f3: 3 });
+
+            using var hook = new Hook(
+                typeof(ReturnBufferSysVTest).GetMethod("SourceNullable", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)!,
+                typeof(ReturnBufferSysVTest).GetMethod("TargetNullable", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)!,
+                true
+            );
+
+            Assert.True(SourceNullable(0, 0, 0, 0) is null);
+        }
+
+        [Fact]
+        public void TestNullableBoolDetour()
+        {
+            var instance = new TestClass();
+            Assert.False(instance.Source());
+
+            using var hook = new Hook(
+                typeof(TestClass).GetMethod("Source", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)!,
+                typeof(TestClass).GetMethod("Target", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)!,
+                true
+            );
+
+            Assert.True(instance.Source());
+        }
+
         internal struct TestStruct
         {
             public ulong f1, f2, f3; // 24 bytes
@@ -37,6 +66,12 @@ namespace MonoMod.UnitTest
             return new TestStruct() { f1 = 1, f2 = 2, f3 = 3 };
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        internal TestStruct? SourceNullable(int a, int b, int c, int d)
+        {
+            return new TestStruct() { f1 = 1, f2 = 2, f3 = 3 };
+        }
+
         internal static TestStruct Target(Func<ReturnBufferSysVTest, int, int, int, int, TestStruct> orig, ReturnBufferSysVTest self, int a, int b, int c, int d)
         {
             var s = orig(self, a, b, c, d);
@@ -44,6 +79,30 @@ namespace MonoMod.UnitTest
             s.f2 += 3;
             s.f3 += 3;
             return s;
+        }
+
+        internal static TestStruct? TargetNullable(Func<ReturnBufferSysVTest, int, int, int, int, TestStruct?> orig, ReturnBufferSysVTest self, int a, int b, int c, int d)
+        {
+            return null;
+        }
+
+        // keep the test in its own class because the existence of fields in the class has significance
+        internal class TestClass
+        {
+            internal static bool? Target(TestClass instance)
+            {
+                _ = instance;
+                return true;
+            }
+
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            internal bool? Source()
+            {
+                return false;
+            }
+
+            // force error by having a field in the test class
+            internal string s1 = "test";
         }
     }
 }
